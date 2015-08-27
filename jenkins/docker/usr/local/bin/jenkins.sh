804628010d9461242e16c6e7568249d9af5541a9
dreay-moj <david.reay@devopsguys.com>
2015-08-27 16:29:17 +0100

    Updates

--- jenkins/docker/usr/local/bin/jenkins.sh
+++ jenkins/docker/usr/local/bin/jenkins.sh
@@ -2,6 +2,32 @@
 
 set -e
 
+# Copy files from /usr/share/jenkins/ref into /var/jenkins_home
+# So the initial JENKINS-HOME is set with expected content. 
+# Don't override, as this is just a reference setup, and use from UI 
+# can then change this, upgrade plugins, etc.
+JENKINS_REF=/srv/jenkins
+N=${#JENKINS_REF}+1
+echo $N
+copy_reference_file() {
+  f=${1%/} 
+  echo "$f" #>> $COPY_REFERENCE_FILE_LOG
+  rel=${f:N}
+  dir=$(dirname ${f})
+  echo " $f -> $rel" #>> $COPY_REFERENCE_FILE_LOG
+  if [[ ! -e ${JENKINS_HOME}/${rel} ]] 
+  then
+    echo "copy $rel to JENKINS_HOME" #>> $COPY_REFERENCE_FILE_LOG
+    mkdir -p ${JENKINS_HOME}/${dir:N}
+    cp -r ${JENKINS_REF}/${rel} ${JENKINS_HOME}/${rel};
+    # pin plugins on initial copy
+    [[ ${rel} == plugins/*.jpi ]] && touch ${JENKINS_HOME}/${rel}.pinned
+  fi; 
+}
+export -f copy_reference_file
+echo "--- Copying files at $(date)" #>> $COPY_REFERENCE_FILE_LOG
+find $JENKINS_REF -type f -exec bash -c "copy_reference_file '{}'" \;
+
 # if `docker run` first argument start with `--` the user is passing jenkins launcher arguments
 if [[ $# -lt 1 ]] || [[ "$1" == "--"* ]]; then
    exec java $JAVA_OPTS -jar /usr/share/jenkins/jenkins.war $JENKINS_OPTS "$@"
